f0c59debb6ffd838f17f87d5ad39fbd4bfb1bba9
Raphael Norris <ext.raphael.norris@amcom.com.br>
2025-09-21 01:32:51 -0300

    feature:script conn

--- project_data_science/src/data/conn_oracle.py
+++ project_data_science/src/data/conn_oracle.py
@@ -1,23 +1,31 @@
 import oracledb
 import os
 from dotenv import load_dotenv
+import sys
 
-load_dotenv()
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
-def load_env_oracle()->str:
-    user = os.getenv("USER")
-    password = os.getenv("PASSWORD")
-    host = os.getenv("HOST")
-    port = os.getenv("PORT", "1521") 
-    service_name = os.getenv("SERVICE_NAME")
 
+
+
+def load_env_oracle() -> oracledb.Connection:
+    user = os.getenv("ORACLE_USER")
+    password = os.getenv("ORACLE_PASSWORD")
+    host = os.getenv("ORACLE_HOST")
+    port = os.getenv("ORACLE_PORT", "1521")
+    service_name = os.getenv("ORACLE_SERVICE_NAME")
+    
+    print(f"User: {user}")
+    print(f"Host: {host}")
+    print(f"Port: {port}")
+    print(f"Service: {service_name}")
 
     conn = oracledb.connect(
         user=user,
         password=password,
-        host =host,
-        port = port,
-        service_name = service_name
+        host=host,
+        port=port,
+        service_name=service_name
     )
     return conn
-  
